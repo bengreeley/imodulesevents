bdc3271b6bbaa2f6a0b82f1d44778c85d351c22a
Ben Greeley <ben.greeley@gmail.com>
2015-01-13 13:56:46 -0500

    formatting cleanup

--- inc/class-imodulesevents.php
+++ inc/class-imodulesevents.php
@@ -4,10 +4,10 @@ if( !class_exists( 'iModulesEvents' ) ) {
 	class iModulesEvents {
 		
 		public function __construct() {
-            add_shortcode('display-sortedrss', array( $this,'render_events') );
-        }
-        
-        /* Grabs iModules event feed, sorts returned items by date that is output in returned item description field... */
+			add_shortcode('display-sortedrss', array( $this,'render_events') );
+		}
+		
+		/* Grabs iModules event feed, sorts returned items by date that is output in returned item description field... */
 		public function render_events ($atts) {
 			$return = '';
 			$dateArray = array();
@@ -28,8 +28,8 @@ if( !class_exists( 'iModulesEvents' ) ) {
 			$rss = fetch_feed( $url );
 			
 			if (!is_wp_error( $rss ) ) {
-			    $maxitems = $rss->get_item_quantity( $count ); 
-			    $rss_items = $rss->get_items( 0, 0 );
+				$maxitems = $rss->get_item_quantity( $count ); 
+				$rss_items = $rss->get_items( 0, 0 );
 			}
 			else {
 				return '';
